0d1dcbc54459c94b2f4cd288a3f01acda4124bb8
sam breslin <samryanbreslin@ymail.com>
2020-03-22 15:48:43 -0400

    fixed differential equasion

--- src/main/java/frc/robot/commands/Driving.java
+++ src/main/java/frc/robot/commands/Driving.java
@@ -11,8 +11,15 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
 public class Driving extends Command {
-  double temp1 = 0;
-  double temp2 = 0;
+  double FL1 = 0;
+  double FL2 = 0;
+  double FR1 = 0;
+  double FR2 = 0;
+  double BL1 = 0;
+  double BL2 = 0;
+  double BR1 = 0;
+  double BR2 = 0;
+
   public Driving() {
     // Use requires() here to declare subsystem dependencies
     requires(Robot.LFmod);
@@ -25,40 +32,50 @@ public class Driving extends Command {
   @Override
   protected void initialize() {
   }
-
+  
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
     //double[] wheelAngles = Robot.calculations.wheelAngles();
     double[] wheelAngles = Robot.calculations.wheelAngles();
     double[] wheelSpeeds = Robot.calculations.wheelSpeeds();
-    
-    double FL1  = Robot.LFmod.getAngle1();
-    double FL2 = Robot.LFmod.getAngle2();
-    double angle = Robot.LFmod.getAngle();
-    Robot.LFmod.setAngle1((wheelSpeeds[0]+FL1)+(wheelAngles[0]-angle));
-    Robot.LFmod.setAngle2((-FL2-wheelSpeeds[0])+(wheelAngles[0]-angle));
-    
-    
-    double BL1  = Robot.LBmod.getAngle1();
-    double BL2 = Robot.LBmod.getAngle2();
-    double angle2 = Robot.LBmod.getAngle();
-    Robot.LBmod.setAngle1((wheelAngles[1]-angle2)+(wheelSpeeds[1]+BL1));
-    System.out.println(BL1 );
-    Robot.LBmod.setAngle2((wheelAngles[1]-angle2)+(-BL2-wheelSpeeds[1]));
-    
-    
-    double FR1  = Robot.FRmod.getAngle1();
-    double FR2 = Robot.FRmod.getAngle2();
-    double angle3 = Robot.FRmod.getAngle();
-    Robot.FRmod.setAngle1((wheelAngles[2]-angle3)+(wheelSpeeds[2]+FR1));
-    Robot.FRmod.setAngle2((wheelAngles[2]-angle3)+(-FR2-wheelSpeeds[2]));
 
-    double BR1  = Robot.BRmod.getAngle1();
-    double BR2 = Robot.BRmod.getAngle2();
-    double angle4 = Robot.BRmod.getAngle();
-    Robot.BRmod.setAngle1((wheelAngles[3]-angle4)+(wheelSpeeds[3]+BR1));
-    Robot.BRmod.setAngle2((wheelAngles[3]-angle4)+(-BR2-wheelSpeeds[3]));
+    
+    
+    //double FL1  = Robot.LFmod.getAngle1();
+    //double FL2 = Robot.LFmod.getAngle2();
+    //double angle = Robot.LFmod.getAngle();
+
+    FL1 = FL1+ wheelSpeeds[0];
+    FL2 = FL2- wheelSpeeds[0];
+    Robot.LFmod.setAngle1((wheelAngles[0])+(FL1));
+    Robot.LFmod.setAngle2((wheelAngles[0])+(FL2));
+    
+    
+    //double BL1  = Robot.LBmod.getAngle1();
+    //double BL2 = Robot.LBmod.getAngle2();
+    //double angle2 = Robot.LBmod.getAngle();
+    BL1 = BL1+ wheelSpeeds[1];
+    BL2 = BL2- wheelSpeeds[1];
+    Robot.LBmod.setAngle1((wheelAngles[1])+(BL1));
+    Robot.LBmod.setAngle2((wheelAngles[1])+(BL2));
+    
+    
+    //double FR1  = Robot.FRmod.getAngle1();
+    //double FR2 = Robot.FRmod.getAngle2();
+    //double angle3 = Robot.FRmod.getAngle();
+    FR1 = FR1+ wheelSpeeds[2];
+    FR2 = FR2- wheelSpeeds[2];
+    Robot.FRmod.setAngle1((wheelAngles[2])+(FR1));
+    Robot.FRmod.setAngle2((wheelAngles[2])+(FR2));
+
+    //double BR1  = Robot.BRmod.getAngle1();
+    //double BR2 = Robot.BRmod.getAngle2();
+    //double angle4 = Robot.BRmod.getAngle();
+    BR1 = BR1+ wheelSpeeds[3];
+    BR2 = BR2- wheelSpeeds[3];
+    Robot.BRmod.setAngle1((wheelAngles[3])+(BR1));
+    Robot.BRmod.setAngle2((wheelAngles[3])+(BR2));
     
     //Robot.LFmod.setAngle1(6);
     //Robot.LFmod.setAngle2(6);
